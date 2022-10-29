a041a9c6a57a08d62d4d002abb24ae76d463ebf2
ruthwekesa <wekesaruth569@gmail.com>
2022-10-29 20:53:44 +0300

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
-
+def oxford_comma(input)
+    if input.length == 1
+        input.join
+    elsif input.length == 2
+        "#{input[0]} and #{input[1]}"
+    else
+    input[-1].insert(0, "and ")
+    input.join(", ")
+    end
 end
